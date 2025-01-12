86991f68d0fe8eea1ac30c5df381113bd4eed339
Hevanafa <dhevansafa4@gmail.com>
2025-01-13 05:37:15 +0700

    added basic text drawing code

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,24 @@
 const std = @import("std");
+const rl = @import("raylib");
+
+const sw = 640;
+const sh = 360;
+
+const cornflowerblue = rl.Color.init(0x64, 0x95, 0xed, 0xff);
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Starting Raylib with Zig...", .{});
+    try stdout.print("Starting Raylib with Zig...\n", .{});
 
+	rl.initWindow(sw, sh, "raylib-zig");
+	rl.setTargetFPS(60);
 
+	while (!rl.windowShouldClose()) {
+		rl.beginDrawing();
+		rl.clearBackground(cornflowerblue);
+		rl.drawText("Hello Raylib ++ Zig!", 20, sh - 100, 36, rl.Color.ray_white);
+		rl.endDrawing();
+	}
+
+	rl.closeWindow();
 }
